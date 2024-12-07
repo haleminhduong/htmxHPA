b216411704f0944b2119f92ca405f3a3de6e6ce4
Le Minh Duong Ha <haleminhduong@gmail.com>
2024-12-07 18:30:44 +0100

    started the main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# main.py
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from datetime import datetime
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/chat", response_class=HTMLResponse)
+async def chat(request: Request):
+    return templates.TemplateResponse("templates/chat/chat.html", {"request": request})
+
+
+@app.get("/journal", response_class=HTMLResponse)
+async def journal(request: Request):
+    return templates.TemplateResponse("templates/journal/journal.html", {"request": request})
+
+
+@app.get("/social", response_class=HTMLResponse)
+async def social(request: Request):
+    return templates.TemplateResponse("templates/social/social.html", {"request": request})
+
+
+@app.get("/mood", response_class=HTMLResponse)
+async def mood(request: Request):
+    return templates.TemplateResponse("templates/mood/mood.html", {"request": request})
+
